6101b66a201549f95f1c9b59d970cd6475e565d2
Михаил <dreminmihail@ysndex.ru>
2022-07-23 16:44:34 +0300

    Add Torchmetric class for Dice

--- callbacks/dice_metric.py
+++ callbacks/dice_metric.py
@@ -1,27 +1,26 @@
 import torch
 
 import pytorch_lightning as pl
-
+from torchmetrics import Metric
 from typing import Dict, Any
 
+class Dice(Metric):
+    def __init__(self, thr: float, smooth: float):
+        super().__init__()
+        self.thr = thr
+        self.smooth = smooth
+        self.add_state("batched_dice", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("total_batches", default=torch.Tensor(0), dist_reduce_fx="sum")
 
-class DiceMetricCallback(pl.Callback):
-    def __init__(self):
-        super(DiceMetricCallback, self).__init__()
-        self.thr = 0.5
-        self.smooth = 1e-7
-
-    def _calculate_dice(
-            self,
-            probs: torch.Tensor,
-            target: torch.Tensor
-    ) -> torch.Tensor:
-        batch_size = probs.shape[0]
-        probs = (probs > self.thr).view(batch_size, -1).float()
-        target = (target > self.thr).view(batch_size, -1).float()
+    def update(self, logits: torch.Tensor, target: torch.Tensor):
+        batch_size = logits.shape[0]
+        probs = (logits >= 0.).view(batch_size, -1).float()
+        target = (target > 0.).view(batch_size, -1).float()
         mult = target * probs
         mult = torch.sum(mult, dim=1)
         denom = torch.sum(probs, dim=1) + torch.sum(target, dim=1)
-        dice = (2 * mult + self.smooth) / (denom + self.smooth)
-        return dice
+        self.batched_dice += (2 * mult + self.smooth) / (denom + self.smooth)
+        self.total_batches += 1
 
+    def compute(self) -> torch.Tensor:
+        return self.batched_dice.float() / self.total_batches
--- holder.py
+++ holder.py
@@ -10,6 +10,7 @@ from typing import (
     Dict, Optional, List, Tuple,
     Callable, Union, Any, Sequence
 )
+from callbacks import Dice
 
 from models.utils import create_model
 
@@ -22,8 +23,7 @@ class ModelHolder(pl.LightningModule):
         super(ModelHolder, self).__init__()
         self.segmentor = create_model(config['model_cfg'])
 
-        self.thr = 0.5
-        self.smooth = 1e-7
+        self.dice = Dice(0.5, 1e-7)
 
     def training_step(
             self,
@@ -59,20 +59,6 @@ class ModelHolder(pl.LightningModule):
         preds = self.forward(input_x)
         return preds
 
-    def _calculate_dice(
-            self,
-            logits: torch.Tensor,
-            target: torch.Tensor
-    ) -> torch.Tensor:
-        batch_size = logits.shape[0]
-        probs = (logits >= 0.).view(batch_size, -1).float()
-        target = (target > 0.).view(batch_size, -1).float()
-        mult = target * probs
-        mult = torch.sum(mult, dim=1)
-        denom = torch.sum(probs, dim=1) + torch.sum(target, dim=1)
-        dice = (2 * mult + self.smooth) / (denom + self.smooth)
-        return torch.mean(dice)
-
     def validation_step(
             self,
             batch_dict: Dict[str, torch.Tensor],
@@ -84,26 +70,13 @@ class ModelHolder(pl.LightningModule):
         target = batch_dict['target']
         #torch.save(probs.cpu(), 'probs.pth')
         #torch.save(target.cpu(), 'target.pth')
-        dice = self._calculate_dice(logits, target)
-        preds.update({
-            'dice': dice,
-            'batch_size': target.shape[0]
-        })
+        self.dice.update(logits, target)
 
     def validation_epoch_end(
             self,
             outputs: Union[Dict[str, torch.Tensor], List[Dict[str, torch.Tensor]]]
     ) -> None:
-        dice_valid_loader = 0
-        if isinstance(outputs, dict):
-            dice_valid_loader = outputs['dice'].item()
-        else:
-            sum = 0
-            for output_valid in outputs:
-                dice_valid_loader += output_valid['dice'].item() * output_valid['batch_size']
-                sum += output_valid['batch_size']
-            dice_valid_loader /= sum + self.smooth
-        self.log('valid_dice', dice_valid_loader, prog_bar=True)
+        self.log('valid_dice', self.dice.compute(), prog_bar=True)
 
     def forward(self, input_x: torch.Tensor) -> Dict[str, Any]:
         logits = self.segmentor(input_x)
