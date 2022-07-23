01ec25a2c4b362c6c51d28032a463109792140c6
Михаил <dreminmihail@ysndex.ru>
2022-07-23 17:45:03 +0300

    Add Torchmetric class for Dice

--- callbacks/dice_metric.py
+++ callbacks/dice_metric.py
@@ -16,7 +16,7 @@ class Dice(Metric):
         print(logits.shape, target.shape)
         batch_size = logits.shape[0]
         probs = (logits >= 0.).view(batch_size, -1).float()
-        target = (target > 0.).view(batch_size, -1).float()
+        target = (target >= 0.).view(batch_size, -1).float()
         mult = target * probs
         mult = torch.sum(mult, dim=1)
         denom = torch.sum(probs, dim=1) + torch.sum(target, dim=1)
@@ -24,7 +24,7 @@ class Dice(Metric):
         print(torch.mean((2 * mult + self.smooth) / (denom + self.smooth)))
         print(batch_size)
         self.sum_dice += torch.sum((2 * mult + self.smooth) / (denom + self.smooth))
-        self.total_samples += torch.Tensor(batch_size)
+        self.total_samples += batch_size
         print("DICE info:", self.sum_dice, self.total_samples)
 
     def compute(self) -> torch.Tensor:
