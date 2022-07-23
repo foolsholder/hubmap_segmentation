0d9c63d635123c29e35339247819405303dbc3b9
Михаил <dreminmihail@ysndex.ru>
2022-07-23 18:08:16 +0300

    Add Torchmetric class for Dice

--- train_model.py
+++ train_model.py
@@ -4,26 +4,32 @@ import os
 from torch.utils.data import DataLoader
 from holder import ModelHolder
 from sdataset import create_loader
+from pytorch_lightning.loggers import WandbLogger
+
 
 config = {
     'model_cfg': {
         'type': 'simple'
+    },
+    'wandb_cfg': {
+        'project': 'hubmap'
     }
 }
 
 model_holder = ModelHolder(config)
-
+wandb_logger = WandbLogger(project=config['wandb_cfg']['project'])
 trainer = pl.Trainer(
     min_epochs=100,
     accelerator='gpu',
     gpus=1,
     num_nodes=1,
     log_every_n_steps=1,
-    weights_save_path=os.environ['SHUBMAP_EXPS']
+    weights_save_path=os.environ['SHUBMAP_EXPS'],
+    logger=wandb_logger
 )
 
 trainer.fit(
     model=model_holder,
-    train_dataloaders=create_loader(train=False),
+    train_dataloaders=create_loader(train=True),
     val_dataloaders=create_loader(train=False),
 )
