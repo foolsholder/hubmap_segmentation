2290872d6e4087ea094cdc7a96b035748df7a11c
Михаил <dreminmihail@ysndex.ru>
2022-07-23 17:10:46 +0300

    Add Torchmetric class for Dice

--- train_model.py
+++ train_model.py
@@ -15,8 +15,8 @@ model_holder = ModelHolder(config)
 
 trainer = pl.Trainer(
     min_epochs=100,
-    accelerator='ddp',
-    gpus=4,
+    accelerator='gpu',
+    gpus=2,
     num_nodes=1,
     log_every_n_steps=1,
     weights_save_path=os.environ['SHUBMAP_EXPS']
