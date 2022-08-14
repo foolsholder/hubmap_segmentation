fdfc800f41d2364cf1f8646d72665c643ccac48a
dreminm <dreminmihail@yandex.ru>
2022-08-14 13:23:42 +0300

    segformer wip

--- hubmap_segmentation/models/utils.py
+++ hubmap_segmentation/models/utils.py
@@ -7,17 +7,19 @@ from .segformer import SegFormer
 def create_model(model_cfg: Dict[str, Union[Dict, Any]]):
 
     #return UNetSegmentor(**model_cfg)
-    return SegFormer(
-    config = model_cfg,
-    in_channels=3,
-    widths=[64, 128, 256, 512],
-    depths=[3, 4, 6, 3],
-    all_num_heads=[1, 2, 4, 8],
-    patch_sizes=[7, 3, 3, 3],
-    overlap_sizes=[4, 2, 2, 2],
-    reduction_ratios=[8, 4, 2, 1],
-    mlp_expansions=[4, 4, 4, 4],
-    decoder_channels=256,
-    scale_factors=[8, 4, 2, 1],
-    num_classes=model_cfg['num_classes'],
-)
+    model = SegFormer(
+        config = model_cfg,
+        in_channels=3,
+        widths=[64, 128, 256, 512],
+        depths=[3, 4, 6, 3],
+        all_num_heads=[1, 2, 4, 8],
+        patch_sizes=[7, 3, 3, 3],
+        overlap_sizes=[4, 2, 2, 2],
+        reduction_ratios=[8, 4, 2, 1],
+        mlp_expansions=[4, 4, 4, 4],
+        decoder_channels=256,
+        scale_factors=[8, 4, 2, 1],
+        num_classes=model_cfg['num_classes'],
+    )
+    model.load_state_dict(torch.load('pretrained_weights.pth'))
+    return model
